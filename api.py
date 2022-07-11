d129eaf2945780312b42374726cc646760c53d8e
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-07-12 00:23:25 +0530

    Delete api.py

--- api.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from collections import OrderedDict
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
-
-info = requests.get('https://contributor-overtime-api.apiseven.com/monthly-contributor?repo=fortran-lang/fpm').text
-print(info)
