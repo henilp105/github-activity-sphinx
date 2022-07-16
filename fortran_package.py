af8480331bdc1225e98663ffc578f094345dbe47
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-07-16 22:17:55 +0530

    Update fortran_package.py

--- fortran_package.py
+++ fortran_package.py
@@ -1,15 +1,26 @@
+import sys
 import yaml
 from pathlib import Path
 from collections import Counter
 import json
 import requests
-import datetime
+from requests.structures import CaseInsensitiveDict
+from datetime import datetime
+import pytz
+from collections import OrderedDict
+import numpy as np
+
+print(str(sys.argv))
+months = ["Unknown", "January","Febuary","March", "April","May","June","July","August","September","October","November","December"]
 #print("learn section")
 info = requests.get('https://raw.githubusercontent.com/fortran-lang/fortran-lang.org/master/_data/package_index.yml').text
 fortran_index = yaml.safe_load(info)
 #print(conf['books'])
 info = requests.get('https://raw.githubusercontent.com/fortran-lang/fortran-lang.org/master/_data/learning.yml').text
 conf = yaml.safe_load(info)
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "Basic aGVuaWxwMTA1OmdocF9TQTZxaHFBYmZVVGNkaEZHc0tnbnllN25vcndUSzk0T0dTVGg="
+#this auth token is only meant for fortran github api use. please DONT misuse it.
 
 fortran_index_tags = []
 fortran_index_tags_50 = []
@@ -74,7 +85,7 @@ fortran_index_categories  = list(set(fortran_index_categories))
 def github_info(list):
   for i in list:
     try:
-        info = requests.get('https://api.github.com/repos/'+i['github']).text
+        info = requests.get('https://api.github.com/repos/'+i['github'], headers=headers).text
         d = json.loads(info)
         if type(d['forks_count']) is type(None):
             d['forks_count'] = 0
@@ -94,12 +105,12 @@ def github_info(list):
         i['forks'] = d['forks_count']
         i['issues'] = d['open_issues_count']
         i['stars'] = d['stargazers_count']
-        info = requests.get('https://api.github.com/repos/'+i['github']+'/commits/'+d['default_branch']).text
+        info = requests.get('https://api.github.com/repos/'+i['github']+'/commits/'+d['default_branch'], headers=headers).text
         d = json.loads(info)
         monthinteger = int(d['commit']['author']['date'][5:7])
-        month = datetime.date(1900, monthinteger, 1).strftime('%B')
+        month = months[monthinteger]
         i['last_commit'] = month+" "+d['commit']['author']['date'][:4]
-        info = requests.get('https://api.github.com/repos/'+i['github']+'/releases/latest').text
+        info = requests.get('https://api.github.com/repos/'+i['github']+'/releases/latest', headers=headers).text
         d = json.loads(info)
         #print(d)
         try:
@@ -139,3 +150,70 @@ with open("fortran_package.json", "w") as f:
     json.dump(fortran_tags, f)
 with open("fortran_learn.json", "w") as f:
     json.dump(conf, f)
+
+fortran_monthly =[]
+fortran_commits =[]
+fpm_monthly =[]
+fpm_commits =[]
+stdlib_monthly =[]
+stdlib_commits =[]
+
+
+def monthly_graph(repo):
+  info = requests.get('https://api.github.com/repos/fortran-lang/'+repo+'/stats/code_frequency', headers=headers).text
+  d = json.loads(info)
+  c = 0
+  monthly_commits=[]
+  commits=[]
+  for i in range(0,len(d),4):
+    for j in range(4):
+      try:
+        c = c + d[i+j][1]
+      except:
+       print("")
+    date_time = datetime.fromtimestamp(d[i][0])
+    monthly = str(months[date_time.month])+" "+str(date_time.year)
+    commits.append(c)
+    monthly_commits.append(monthly)
+    c=0
+  test_chart = {"data": [
+      {
+        "x": monthly_commits,
+        "y": commits,
+      }
+    ],
+    "layout": {
+      "margin": {
+        "t": 15,
+        "b": 30,
+        "r": 15,
+        "l": 35
+      }
+    }
+  }
+  with open("source/charts/"+repo+".json", "w") as f:
+        json.dump(test_chart, f)
+graphs =["fortran-lang.org","fpm","stdlib"]
+for i in graphs:
+  monthly_graph(i)
+
+contributor =[]
+contributor_repo={
+    "repo":'fortran-lang',
+}
+def contributors(repo):
+  info = requests.get('https://api.github.com/repos/'+repo+'/contributors', headers=headers).text
+  d = json.loads(info)
+  for i in d:
+    contributor.append(i['login'])
+
+graphs =["fortran-lang/fortran-lang.org","fortran-lang/fpm","fortran-lang/stdlib","j3-fortran/fortran_proposals"]
+for i in graphs:
+  contributors(i)
+
+contributor = list(set(contributor))
+contributor.sort()
+contributor_repo['contributor'] = contributor
+
+with open("contributor.json", "w") as f:
+  json.dump(contributor_repo, f)
